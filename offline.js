23d5901049ac35a40ed5e6d7ef1e4ec6957758ea
Christian Collins <99655989+Dabluew@users.noreply.github.com>
2023-04-15 17:50:15 -0400

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1681595151,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/click-sheet0.png",
+		"images/bg-sheet0.png",
+		"images/particles.png",
+		"images/shopb-sheet0.png",
+		"images/exit-sheet0.png",
+		"images/exit-sheet1.png",
+		"images/particles2.png",
+		"images/load-sheet0.png",
+		"images/load-sheet1.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"pixeled.woff",
+		"pixeled.woff2",
+		"stylesheet.css"
+	]
+}
